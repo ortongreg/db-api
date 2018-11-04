9666d1b768d61122e8cd29f0f1aa375fdce60dfa
ortongreg <gorton@pillartechnology.com>
2018-11-04 16:57:20 -0600

    adding empHistory and workSchedule tables

---
+++ src/main/resources/db/migration/dbo/V1__Create_Dbo.sql
@@ -0,0 +1,31 @@
+create table dbo.Person (
+    ID int not null,
+    NAME varchar(100) not null,
+    PRIMARY KEY (ID)
+);
+
+create table dbo.Location (
+    ID int not null,
+    NAME varchar(100) not null,
+);
+
+create table dbo.EmploymentHistory (
+    EMPLOYEENUM int not null,
+    PERSON int not null,
+    WAGE float not null,
+    LOCATION int not null,
+    STARTDATE DATE not null,
+    ENDDATE DATE null,
+    PRIMARY KEY (EMPLOYEENUM),
+    FOREIGN KEY (PERSON) REFERENCES dbo.Person(ID),
+    FOREIGN KEY (LOCATION) REFERENCES dbo.Location(ID),
+);
+
+create table dbo.WorkSchedule (
+    ID int not null,
+    PERSION int not null,
+    DATE date not null,
+    HOURS int not null,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (PERSON) REFERENCES dbo.Person(ID)
+);
