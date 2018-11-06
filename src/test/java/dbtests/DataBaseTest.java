c556de47327815eb809e6918eb1d6a9a885322c7
ortongreg <gorton@pillartechnology.com>
2018-11-05 18:50:09 -0600

    helpers on database test

--- src/test/java/dbtests/DataBaseTest.java
+++ src/test/java/dbtests/DataBaseTest.java
@@ -1,7 +1,6 @@
 package dbtests;
 
 import org.junit.Before;
-import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -10,9 +9,10 @@ import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 public class DataBaseTest {
-    protected JdbcTemplate jdbcTemplate;
+    JdbcTemplate jdbcTemplate;
     private PlatformTransactionManager transactionManager;
 
     @Before
@@ -27,6 +27,17 @@ public class DataBaseTest {
 
         jdbcTemplate = new JdbcTemplate(dataSource);
         transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+
+    void insert(String tableName, List<String> cols, String... values){
+        String columns = "("+String.join(",", cols)+")";
+        String valuesPart = "('" + String.join("','", values) +"')";
+        String sql = "INSERT INTO " + tableName + " " + columns +" VALUES " + valuesPart + ";";
+        jdbcTemplate.execute(sql);
+    }
+
+    List<Map<String, Object>> queryForList(String query){
+        return jdbcTemplate.queryForList(query);
     }
 
 }
--- src/test/java/dbtests/PersonTest.java
+++ src/test/java/dbtests/PersonTest.java
@@ -1,24 +1,23 @@
 package dbtests;
 
 import org.junit.Test;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class PersonTest extends DataBaseTest {
 
     @Test
     public void givenNoPersons_WhenSelectAllPerson_thenRetrunZeroPersons()  {
-
-        List l = jdbcTemplate.queryForList("select * from dbo.Person");
-
+        List l = queryForList("select * from dbo.Person");
         assert 0 == l.size();
     }
 
     @Test
     public void givenOnePersons_WhenSelectAllPerson_thenRetrunOnePersons()  {
+        insert("dbo.Person", Arrays.asList("Name"), "Bob Jones");
 
-        jdbcTemplate.execute("insert into dbo.Person (Name) values ('Bob Jones')");
-        List l = jdbcTemplate.queryForList("select * from dbo.Person");
-
+        List l = queryForList("select * from dbo.Person");
         assert 1 == l.size();
     }
 
