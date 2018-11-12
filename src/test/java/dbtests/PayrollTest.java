e7836725263823b8ed0b0c87963280dc269ae494
ortongreg <gorton@pillartechnology.com>
2018-11-12 17:04:54 -0600

    multiple days worked

--- src/test/java/dbtests/PayrollTest.java
+++ src/test/java/dbtests/PayrollTest.java
@@ -30,4 +30,23 @@ public class PayrollTest extends DataBaseTest {
         assert "160.0".equals(row.get("AMOUNT").toString());
     }
 
+    @Test
+    public void givenOnePersons_MultipleWorkDay_WhenSelectSp_Payroll_thenRetrunOneRows()  {
+        String name = "Bob Jones";
+        String location = "homebase";
+        insertPerson(name);
+        insertWorkSchedule(name, "2018-11-02", 8);
+        insertWorkSchedule(name, "2018-11-03", 4);
+        insertLocation(location);
+        insertEmploymentHistory(name, 20, location, "2018-10-26", "2018-11-09");
+
+        List result = queryForList("call dbapi.sp_Payroll('2018-11-09')");
+        assert 1 == result.size();
+        Map<Object, Object> row = (Map<Object, Object>) result.get(0);
+        assert name.equals(row.get("NAME"));
+        assert "12".equals(row.get("HOURS").toString());
+        assert "2018-11-09".equals(row.get("END_DATE"));
+        assert "240.0".equals(row.get("AMOUNT").toString());
+    }
+
 }
