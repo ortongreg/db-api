1af50a739515a72601912da9950ededacc16d2ad
ortongreg <gorton@pillartechnology.com>
2018-11-12 19:05:00 -0600

    refactor test

--- src/main/resources/db/migration/dbapi/V2__Create_sp_Payroll.sql
+++ src/main/resources/db/migration/dbapi/V2__Create_sp_Payroll.sql
@@ -1,12 +1,13 @@
 DELIMITER //
 CREATE PROCEDURE dbapi.sp_Payroll
-(IN startdt DATE)
+(IN startdt DATE, IN enddt DATE)
 BEGIN
 
 
   select P.NAME,
     (SELECT SUM(HOURS) FROM dbo.WorkSchedule WS WHERE WS.PERSON = P.ID) AS HOURS,
-    '2018-11-09' AS END_DATE,
+    startdt AS START_DATE,
+    enddt AS END_DATE,
     (SELECT WAGE FROM dbo.EmploymentHistory EH WHERE EH.PERSON = P.ID)*(SELECT SUM(HOURS) FROM dbo.WorkSchedule WS WHERE WS.PERSON = P.ID) AS AMOUNT
   from dbo.Person P;
 
--- src/test/java/dbtests/PayrollTest.java
+++ src/test/java/dbtests/PayrollTest.java
@@ -1,19 +1,27 @@
 package dbtests;
 
 import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
 public class PayrollTest extends DataBaseTest {
+    private static final LocalDate OCT_26 = LocalDate.of(2018, 10,26);
+    private static final LocalDate NOV_9 = LocalDate.of(2018, 11,9);
 
     @Test
-    public void givenNoWorkScheduled_WhenSp_Payroll_thenRetrunNoRows()  {
-        List l = queryForList("call dbapi.sp_Payroll('2018-11-01')");
+    public void givenNoWorkScheduled_WhenSp_Payroll_thenReturnNoRows()  {
+        LocalDate start = LocalDate.of(2018, 11,1);
+        LocalDate end = LocalDate.of(2018, 11,5);
+        List l = payrollQuery(start, end);
+
         assert 0 == l.size();
     }
 
     @Test
-    public void givenOnePersons_oneWorkDay_WhenSelectSp_Payroll_thenRetrunOneRows()  {
+    public void givenOnePersons_oneWorkDay_WhenSelectSp_Payroll_thenReturnOneRows()  {
         String name = "Bob Jones";
         String location = "homebase";
         insertPerson(name);
@@ -21,17 +29,18 @@ public class PayrollTest extends DataBaseTest {
         insertLocation(location);
         insertEmploymentHistory(name, 20, location, "2018-10-26", "2018-11-09");
 
-        List result = queryForList("call dbapi.sp_Payroll('2018-11-09')");
+        List result = payrollQuery(OCT_26, NOV_9);
         assert 1 == result.size();
         Map<Object, Object> row = (Map<Object, Object>) result.get(0);
         assert name.equals(row.get("NAME"));
         assert "8".equals(row.get("HOURS").toString());
-        assert "2018-11-09".equals(row.get("END_DATE"));
+        assert "2018-10-26".equals(row.get("START_DATE").toString());
+        assert "2018-11-09".equals(row.get("END_DATE").toString());
         assert "160.0".equals(row.get("AMOUNT").toString());
     }
 
     @Test
-    public void givenOnePersons_MultipleWorkDay_WhenSelectSp_Payroll_thenRetrunOneRows()  {
+    public void givenOnePersons_MultipleWorkDay_WhenSelectSp_Payroll_thenReturnOneRows()  {
         String name = "Bob Jones";
         String location = "homebase";
         insertPerson(name);
@@ -40,12 +49,12 @@ public class PayrollTest extends DataBaseTest {
         insertLocation(location);
         insertEmploymentHistory(name, 20, location, "2018-10-26", "2018-11-09");
 
-        List result = queryForList("call dbapi.sp_Payroll('2018-11-09')");
+        List result = payrollQuery(OCT_26, NOV_9);
         assert 1 == result.size();
         Map<Object, Object> row = (Map<Object, Object>) result.get(0);
         assert name.equals(row.get("NAME"));
         assert "12".equals(row.get("HOURS").toString());
-        assert "2018-11-09".equals(row.get("END_DATE"));
+        assert "2018-11-09".equals(row.get("END_DATE").toString());
         assert "240.0".equals(row.get("AMOUNT").toString());
     }
 
@@ -64,18 +73,25 @@ public class PayrollTest extends DataBaseTest {
         insertWorkSchedule(sally, "2018-11-04", 4);
         insertEmploymentHistory(sally, 22, location, "2017-01-01", null);
 
-        List result = queryForList("call dbapi.sp_Payroll('2018-11-09')");
+        List result = payrollQuery(OCT_26, NOV_9);
         assert 2 == result.size();
         Map<Object, Object> bobRow = (Map<Object, Object>) result.get(0);
         assert bob.equals(bobRow.get("NAME"));
         assert "8".equals(bobRow.get("HOURS").toString());
-        assert "2018-11-09".equals(bobRow.get("END_DATE"));
+        assert "2018-11-09".equals(bobRow.get("END_DATE").toString());
         assert "160.0".equals(bobRow.get("AMOUNT").toString());
         Map<Object, Object> sallyRow = (Map<Object, Object>) result.get(1);
         assert sally.equals(sallyRow.get("NAME"));
         assert "4".equals(sallyRow.get("HOURS").toString());
-        assert "2018-11-09".equals(sallyRow.get("END_DATE"));
+        assert "2018-11-09".equals(sallyRow.get("END_DATE").toString());
         assert "88.0".equals(sallyRow.get("AMOUNT").toString());
+    }
+
+    private List<Map<String, Object>> payrollQuery(LocalDate startDate, LocalDate endDate){
+        LocalDate ldStart = startDate.plusDays(1);
+        LocalDate ldEnd = endDate.plusDays(1);
+        DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_DATE;
+        return queryForList("call dbapi.sp_Payroll('"+ ldStart.format(dtf) +"','"+ ldEnd.format(dtf) +"')");
     }
 
 }
