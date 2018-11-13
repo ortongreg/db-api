6752d926810966bba5992d6df281c7caf63759be
ortongreg <gorton@pillartechnology.com>
2018-11-12 22:48:39 -0600

    sp calls fn

--- src/main/resources/db/migration/dbapi/V2__Create_sp_Payroll.sql
+++ src/main/resources/db/migration/dbapi/V2__Create_sp_Payroll.sql
@@ -8,7 +8,7 @@ BEGIN
     (SELECT SUM(HOURS) FROM dbo.WorkSchedule WS WHERE WS.PERSON = P.ID AND startdt <= WS.DATE) AS HOURS,
     startdt AS START_DATE,
     enddt AS END_DATE,
-    (SELECT WAGE FROM dbo.EmploymentHistory EH WHERE EH.PERSON = P.ID)
+    (select dbapi.fn_Wage(P.NAME))
       *(SELECT SUM(HOURS) FROM dbo.WorkSchedule WS WHERE WS.PERSON = P.ID AND startdt <= WS.DATE) AS AMOUNT
   from dbo.Person P;
 
